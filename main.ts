fae3fd55518eedcddcf55c1773a499a07150c798
bexmann <99690783+bexmann@users.noreply.github.com>
2022-02-25 17:28:24 +0100

    "Shut down" and wait 2 seconds

--- main.ts
+++ main.ts
@@ -2,4 +2,7 @@ basic.forever(function () {
     basic.showIcon(IconNames.Heart)
     basic.setLedColor(0xff0000)
     music.playMelody("C5 B A G F E D C ", 120)
+    basic.clearScreen()
+    basic.turnRgbLedOff()
+    basic.pause(2000)
 })
